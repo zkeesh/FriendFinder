9af9b7d46521829902b6367de2f6adedc8a1d0c7
Zachary <zkeesh@gmail.com>
2017-06-04 21:53:51 -0500

    update survey questions

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,7 +10,6 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
 
-  // If no matching route is found default to home
   app.use(function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
